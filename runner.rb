b2b257aa255fa71a7cff5a3ef103099558ecabda
Torey Hickman <torey@toreyhickman.com>
2016-09-06 13:54:39 -0400

    Moves behavior from runner to model

--- runner.rb
+++ runner.rb
@@ -8,5 +8,5 @@ people = PersonParser.parse('people.csv')
 # Get people with phone numbers in a certain area code
 puts "The following people have phone numbers from area code 419."
 
-people_with_area_code_419 = people.select { |person| /\A1-419/.match person.phone }
-people_with_area_code_419.each { |person| puts "#{person.first_name} #{person.last_name}" }
+people_with_area_code_419 = people.select { |person| person.area_code?("419") }
+people_with_area_code_419.each { |person| puts person.full_name }
